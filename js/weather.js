099a2f002028200b11b74e5b057e9b13eeb8d19e
Nate Palmer <nate@natedpalm.com>
2021-01-28 20:38:35 -0500

    Update to my stuff

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '69cbb6389f1610ece18f1f2f1b1a466f';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 33.896019;
+  let longitude = -84.456235;
 
   getWeather(latitude, longitude);
 }
